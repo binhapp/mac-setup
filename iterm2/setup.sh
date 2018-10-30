c97c41f3d2f3108ad3f926e143faebb2254b2561
Binh Nguyen <blcsntb@gmail.com>
2018-10-30 17:35:29 +0700

    Fix font link

--- iterm2/setup.sh
+++ iterm2/setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
 cd ~/.oh-my-zsh/custom/themes/powerlevel9k
+
 curl https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/schemes/Solarized%20Dark%20-%20Patched.itermcolors > "Solarized Dark - Patched.itermcolors"
-curl https://raw.githubusercontent.com/powerline/fonts/blob/master/SourceCodePro/Source%20Code%20Pro%20for%20Powerline.otf > "Source Code Pro for Powerline.otf"
+open "Solarized Dark - Patched.itermcolors"
+
+curl https://raw.githubusercontent.com/powerline/fonts/master/SourceCodePro/Source%20Code%20Pro%20for%20Powerline.otf > "Source Code Pro for Powerline.otf"
 open "Source Code Pro for Powerline.otf"
+
 cd -
