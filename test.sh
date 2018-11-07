680a1fbae58a1f12c59aa3cee91e93740831e59f
Binh Nguyen <blcsntb@gmail.com>
2018-11-07 12:04:42 +0700

    Remove test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "# Install"
-./install.sh
-[ "$CI" ] && curl https://raw.githubusercontent.com/blcsntb/mac-setup/master/install.sh | bash
-
-echo "# Config"
-./config.sh
-[ "$CI" ] && curl https://raw.githubusercontent.com/blcsntb/mac-setup/master/config.sh | bash
