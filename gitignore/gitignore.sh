7b6222015cc764d24598a35bfd184bcc14060171
Binh Nguyen <blcsntb@gmail.com>
2018-11-12 16:48:48 +0700

    Check exist ~/.gitignore_global

--- gitignore/gitignore.sh
+++ gitignore/gitignore.sh
@@ -3,7 +3,7 @@
 $MAC_SETUP_DIR/install.sh gitignore \
   --script-version "" \
   --script-config "
-    rm ~/.gitignore_global
+    [ -f ~/.gitignore_global ] && rm ~/.gitignore_global
     ln -s $MAC_SETUP_DIR/gitignore/gitignorerc ~/.gitignore_global
     ls -la ~/.gitignore_global" \
   $@
