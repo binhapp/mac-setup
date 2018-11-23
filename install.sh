0f12b97ab1a33a6246b3b89d7dd3ce58213b387e
Binh Nguyen <blcsntb@gmail.com>
2018-11-23 10:06:00 +0700

    Fix clone path

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-[ ! -d ~/mac-setup ] && git clone https://github.com/blcsntb/mac-setup
+[ ! -d ~/mac-setup ] && git clone https://github.com/blcsntb/mac-setup ~/mac-setup
 ~/mac-setup/mac-setup --install
