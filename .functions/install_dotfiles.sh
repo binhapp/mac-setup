3567e4813b4ca5a519628ec2bb34d718336d9bf1
Binh Nguyen <blcsntb@gmail.com>
2019-04-25 01:19:46 +0700

    Remove alias dotfiles

---
+++ .functions/install_dotfiles.sh
@@ -0,0 +1,15 @@
+install_dotfiles() {
+  clone_z
+  download_fonts
+  link_dotfiles
+  install_oh_my_zsh
+  install_homebrew
+  install_rbenv
+  install_nvm
+  source ~/.zshrc
+  # brew_cask_install
+  brew_install
+  pip3_install && source ~/.zshrc
+  gem_install
+  npm_install
+}
--- dotfiles.sh
+++ dotfiles.sh
@@ -14,20 +14,3 @@ update_dotfiles_repo() {
 }
 
 for f in $DOTFILES_DIR/.functions/*; do source $f; done
-
-alias install_dotfiles='
-  clone_dotfiles_repo
-  clone_z
-  download_fonts
-  link_dotfiles
-  install_oh_my_zsh
-  install_homebrew
-  install_rbenv
-  install_nvm
-  source ~/.zshrc
-  # brew_cask_install
-  brew_install
-  pip3_install && source ~/.zshrc
-  gem_install
-  npm_install
-'
