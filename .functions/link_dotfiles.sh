ce6c5966df5ca85dc9fa7cc5bcacd44483ca834d
Binh Nguyen <blcsntb@gmail.com>
2019-04-25 12:00:39 +0700

    Add shell function link_file

--- .functions/link_dotfiles.sh
+++ .functions/link_dotfiles.sh
@@ -1,55 +1,16 @@
 link_dotfiles() {
-  DOTFILES_DIR=~/.dotfiles
+  link_file .zshrc
+  link_file .vimrc
+  link_file .ruby-version
+  link_file .gemrc
+  link_file .nvmrc
+  link_file .vmdrc
+  link_file .gitignore_global
 
-  # zshrc
-  [ -f ~/.zshrc ] && rm ~/.zshrc
-  ln -s $DOTFILES_DIR/.zshrc ~
+  link_file .config/powerline-shell/config.json
 
-  # vimrc
-  [ -f ~/.vimrc ] && rm ~/.vimrc
-  ln -s $DOTFILES_DIR/.vimrc ~
-
-  # ruby-version
-  [ -f ~/.ruby-version ] && rm ~/.ruby-version
-  ln -s $DOTFILES_DIR/.ruby-version ~
-
-  # gemrc
-  [ -f ~/.gemrc ] && rm ~/.gemrc
-  ln -s $DOTFILES_DIR/.gemrc ~
-
-  # nvmrc
-  [ -f ~/.nvmrc ] && rm ~/.nvmrc
-  ln -s $DOTFILES_DIR/.nvmrc ~
-
-  # vmdrc
-  [ -f ~/.vmdrc ] && rm ~/.vmdrc
-  ln -s $DOTFILES_DIR/.vmdrc ~
-
-  # gitignore_global
-  [ -f ~/.gitignore_global ] && rm ~/.gitignore_global
-  ln -s $DOTFILES_DIR/.gitignore_global ~
-
-  # powerline-shell
-  DIR=.config/powerline-shell
-  mkdir -p ~/$DIR
-  [ -f ~/$DIR/config.json ] && rm ~/$DIR/config.json
-  ln -s $DOTFILES_DIR/$DIR/config.json ~/$DIR
-
-  # iterm2
-  DIR=Library/Preferences
-  cp $DOTFILES_DIR/$DIR/com.googlecode.iterm2.plist ~/$DIR
-
-  # spectacle
-  DIR="Library/Application Support/Spectacle"
-  mkdir -p "$HOME/$DIR"
-  [ -f "$HOME/$DIR/Shortcuts.json" ] && rm "$HOME/$DIR/Shortcuts.json"
-  ln -s "$DOTFILES_DIR/$DIR/Shortcuts.json" "$HOME/$DIR"
-
-  # vscode
-  DIR="Library/Application Support/Code/User"
-  mkdir -p "$HOME/$DIR"
-  [ -f "$HOME/$DIR/settings.json" ] && rm "$HOME/$DIR/settings.json"
-  [ -f "$HOME/$DIR/keybindings.json" ] && rm "$HOME/$DIR/keybindings.json"
-  ln -s "$DOTFILES_DIR/$DIR/settings.json" "$HOME/$DIR"
-  ln -s "$DOTFILES_DIR/$DIR/keybindings.json" "$HOME/$DIR"
+  link_file Library/Application\ Support/Spectacle
+  link_file Library/Application\ Support/Code/User/settings.json
+  link_file Library/Application\ Support/Code/User/keybindings.json
+  link_file Library/Preferences/com.googlecode.iterm2.plist
 }
