00478a970522ab5f6d50c23c42f183c2700f3df2
Binh Nguyen <blcsntb@gmail.com>
2019-04-25 15:46:00 +0700

    Fix typo

--- .functions/sync_brew_cask_list.sh
+++ .functions/sync_brew_cask_list.sh
@@ -1,4 +1,4 @@
-sync_brew_cast_list() {
+sync_brew_cask_list() {
   casks=(`cat $DOTFILES_DIR/.config/brew_cask_list.txt`)
   for cask in "${casks[@]}"; do
     brew cask install $cask || brew cask install $cask --force
