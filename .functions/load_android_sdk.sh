503e7e6781a462bce6f4d85a2cccaba254cfe097
Binh Nguyen <blcsntb@gmail.com>
2019-04-25 22:12:59 +0700

    unset ANDROID_HOME if not install

--- .functions/load_android_sdk.sh
+++ .functions/load_android_sdk.sh
@@ -1,6 +1,7 @@
 load_android_sdk() {
   export ANDROID_HOME=$HOME/Library/Android/sdk
   if [ ! -d "$ANDROID_HOME" ]; then
+    unset ANDROID_HOME
     return
   fi
   export PATH=$ANDROID_HOME/tools:$PATH
