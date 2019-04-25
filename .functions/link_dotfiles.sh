d3cf9596253384a8ea1a305cf3fb520ecef5cf2c
Binh Nguyen <blcsntb@gmail.com>
2019-04-25 22:29:32 +0700

    Fix wrong link config spectacle

--- .functions/link_dotfiles.sh
+++ .functions/link_dotfiles.sh
@@ -7,7 +7,7 @@ link_dotfiles() {
   link_file .vmdrc
   link_file .gitignore_global
   link_file .config/powerline-shell/config.json
-  link_file Library/Application\ Support/Spectacle
+  link_file Library/Application\ Support/Spectacle/Shortcuts.json
   link_file Library/Application\ Support/Code/User/settings.json
   link_file Library/Application\ Support/Code/User/keybindings.json
   link_file Library/Preferences/com.googlecode.iterm2.plist
